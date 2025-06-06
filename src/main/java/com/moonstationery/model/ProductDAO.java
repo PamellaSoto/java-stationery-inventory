25da0813eee309131ded79c05f1a3c810f4953ab
Pamella Lopes <114709755+pamesss@users.noreply.github.com>
2025-06-06 15:07:24 -0300

    Updated project CSS

--- src/main/java/com/moonstationery/model/ProductDAO.java
+++ src/main/java/com/moonstationery/model/ProductDAO.java
@@ -23,21 +23,43 @@ public class ProductDAO {
 	}
 
     public void insertProduct(Product product) {
-       String query = "INSERT INTO product(product_name, product_description, price, stock, image_url, anime_id, product_type_id, is_available) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
-       Object[] values = new Object[8];
-        values[0] = product.getProduct_name();
-        values[1] = product.getProduct_description();
-        values[2] = product.getPrice();
-        values[3] = product.getStock();
-        values[4] = product.getImage_url();
-        values[5] = product.getAnime_id();
-        values[6] = product.getProduct_type_id();
-        values[7] = product.getIs_available();
+        String query = """
+            INSERT INTO product(
+                            product_name, 
+                            product_description, 
+                            price, 
+                            stock, 
+                            image_url, 
+                            anime_id, 
+                            product_type_id, 
+                            is_available)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+        """;
+        Object[] values = new Object[8];
+            values[0] = product.getProduct_name();
+            values[1] = product.getProduct_description();
+            values[2] = product.getPrice();
+            values[3] = product.getStock();
+            values[4] = product.getImage_url();
+            values[5] = product.getAnime_id();
+            values[6] = product.getProduct_type_id();
+            values[7] = product.getIs_available();
         jdbc.update(query, values);
     }
 
     public void editProduct(Product product) {
-       String query = "UPDATE product SET product_name = ?, product_description = ?, price = ?, stock = ?, image_url = ?, anime_id = ?, product_type_id = ?, is_available = ? WHERE id = ?;";
+       String query = """
+            UPDATE product 
+                SET product_name = ?, 
+                    product_description = ?, 
+                    price = ?, 
+                    stock = ?, 
+                    image_url = ?, 
+                    anime_id = ?, 
+                    product_type_id = ?, 
+                    is_available = ? 
+            WHERE id = ?;
+        """;
        Object[] values = new Object[9];
         values[0] = product.getProduct_name();
         values[1] = product.getProduct_description();
@@ -57,11 +79,40 @@ public class ProductDAO {
     }
 
     public List<Map<String,Object>> listAllAvailable() {
-        String query = "SELECT product_name, product_description, price, stock, image_url, anime_id, product_type_id, is_available FROM product WHERE is_available = true;";
+        String query = """
+            SELECT 
+                p.product_name,
+                p.product_description,
+                p.price,
+                p.stock,
+                p.image_url,
+                p.is_available,
+                a.name AS anime_name,
+                pt.name AS product_type_name
+            FROM product p
+            JOIN anime a ON p.anime_id = a.id
+            JOIN product_type pt ON p.product_type_id = pt.id;
+            WHERE p.is_available = true;
+        """;
         return jdbc.queryForList(query);
     }
+
     public List<Map<String,Object>> listAll() {
-        String query = "SELECT id, product_name, product_description, price, stock, image_url, anime_id, product_type_id, is_available FROM product;";
+        String query = """
+            SELECT 
+                p.id,
+                p.product_name,
+                p.product_description,
+                p.price,
+                p.stock,
+                p.image_url,
+                p.is_available,
+                a.name AS anime_name,
+                pt.name AS product_type_name
+            FROM product p
+            JOIN anime a ON p.anime_id = a.id
+            JOIN product_type pt ON p.product_type_id = pt.id;
+        """;
         return jdbc.queryForList(query);
     }
 }
