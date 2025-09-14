ea2af2ea34a9ec122181b26597fe36559be7701b
Pamella Sotomayor <pamellasoto.dev@gmail.com>
2025-09-14 12:49:16 -0300

    update (render): config code to be able to deploy the project

---
+++ src/main/resources/data-postgresql.sql
@@ -0,0 +1,18 @@
+INSERT INTO anime (name, slug) VALUES 
+('Sailor Moon', 'sailor-moon'),
+('One Piece', 'one-piece'),
+('Pokémon', 'pokemon'),
+('Naruto', 'naruto'),
+('Snoopy', 'snoopy'),
+('Studio Ghibli', 'studio-ghibli'),
+('Sanrio', 'sanrio');
+
+INSERT INTO product_type (name, slug) VALUES 
+('Pens', 'pens'),
+('Notebooks', 'notebooks'),
+('Pencil Cases', 'pencil-cases'),
+('Keychains', 'keychains'),
+('Pencils', 'pencils'),
+('Greeting Cards', 'greeting-cards'),
+('Stamps', 'stamps'),
+('Stickers', 'stickers');
